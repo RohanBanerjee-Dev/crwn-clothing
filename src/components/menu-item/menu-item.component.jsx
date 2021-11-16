70a1829344bc48728d5c0075f4377273851891f6
Ayan Banerjee <ayanbanerjee102@gmail.com>
2021-11-16 15:50:39 +0530

    update our menu-items to navigate using useNavigate HOC

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => (
-    <div  className={`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, size, linkUrl }) => {
+    const navigate = useNavigate();
+    return (
+        <div  className={`${size} menu-item`} onClick={() => navigate(`${linkUrl}`)} >
         <div className='background-image' style={{
         backgroundImage: `url(${imageUrl})`
         }} />
@@ -11,7 +14,8 @@ const MenuItem = ({ title, imageUrl, size }) => (
             <h1 className='title'>{title.toUpperCase() }</h1>
             <span className='subtitle'>SHOP NOW</span>
         </div>
-    </div>
-)
+        </div>
+    )
+ }
 
-export default MenuItem; 
+export default MenuItem;
